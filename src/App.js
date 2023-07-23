396cdf6490e851bcab051ee90066bcb089b0add0
Eeshan Tripathii <eeshan@mit.edu>
2023-07-23 19:44:52 -0400

    Adds dropdown for chosing road with some functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
 
 import Schedule from "./components/Schedule";
@@ -19,12 +19,13 @@ const App = () => {
   return (
     <Router>
       <div className="App">
+        <Dropdown />
         <header className="App-header">
           <NavigationMenu />
           <div className="login-container">
             <img className="login-image" src={tim} alt="Login" />
             <button className="login-button">login</button>
-          </div>
+          </div>          
         </header>
         <Routes>
           <Route path="/" element={<Schedule />} />
@@ -33,6 +34,63 @@ const App = () => {
         </Routes>
       </div>
     </Router>
+  );
+};
+
+const Dropdown = () => {
+  const [selectedRoad, setSelectedRoad] = useState("Road 1");
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const handleRoadChange = (option) => {
+    setSelectedRoad(option);
+    setIsDropdownOpen(false);
+  };
+
+  const handleEditRoad = (option) => {
+    // Handle edit option logic here
+  };
+
+  const handleCopyRoad = (option) => {
+    // Handle copy option logic here
+  };
+
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsDropdownOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div className="dropdown" ref={dropdownRef}>
+      <div className="dropdown-input" onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+        {selectedRoad}
+      </div>
+      <div className={`dropdown-menu ${isDropdownOpen ? "dropdown-menu-open" : ""}`}>
+        <div className="dropdown-option">
+          <span onClick={() => handleRoadChange("Road 1")}>Road 1</span>
+          <button onClick={() => handleEditRoad("Road 1")}>Edit</button>
+          <button onClick={() => handleCopyRoad("Road 1")}>Copy</button>
+        </div>
+        <div className="dropdown-option">
+          <span onClick={() => handleRoadChange("Road 2")}>Road 2</span>
+          <button onClick={() => handleEditRoad("Road 2")}>Edit</button>
+          <button onClick={() => handleCopyRoad("Road 2")}>Copy</button>
+        </div>
+        <div className="dropdown-option">
+          <span onClick={() => handleRoadChange("Road 3")}>Road 3</span>
+          <button onClick={() => handleEditRoad("Road 3")}>Edit</button>
+          <button onClick={() => handleCopyRoad("Road 3")}>Copy</button>
+        </div>
+      </div>
+    </div>
   );
 };
 
